e2defbbe604939b6c19974dd16f3ef826532ff4c
KalyanMurapaka45 <kalyanmurapaka274@gmail.com>
2024-04-27 11:06:15 +0530

    Project Essentials Update

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'SafeGaurd',
+    version= '0.0.0',
+    author= 'Hema Kalyan',
+    author_email= 'mhemakalyan@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
